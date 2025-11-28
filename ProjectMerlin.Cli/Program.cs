e1ab1ddfb7d36edae07274efdcd3942ddaa34347
Lander <55660647+FelixLeander@users.noreply.github.com>
2025-11-28 22:29:08 +0100

    removed ttest

--- ProjectMerlin.Cli/Program.cs
+++ ProjectMerlin.Cli/Program.cs
@@ -1,10 +1,5 @@
 using ProjectMerlin.Cli.Business;
-using System.Drawing;
 
-
-var coolor = ColorTranslator.FromHtml("#0496C7");
-
-return 7;
 try
 {
     return await ConsoleMenues.MainMenuAsync();
